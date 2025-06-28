cb317513bac6803801d022dd0c3893dc0b4e8ea8
Durga Prasad <durgaaprasadd765@gmail.com>
2025-06-28 11:12:36 +0530

    Create TecheazyDevopsApplication.java

---
+++ src/main/java/com/example/techeazydevops/TecheazyDevopsApplication.java
@@ -0,0 +1,11 @@
+package com.example.techeazydevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheazyDevopsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TecheazyDevopsApplication.class, args);
+    }
+}
